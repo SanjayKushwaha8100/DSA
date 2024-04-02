58935a742345a191b646f881bbbc419d23d7612b
goluk <goluk@DESKTOP-JMIKSHT>
2024-04-02 22:34:05 +0530

    ok

--- src/sortingalgo/BubbleSort.java
+++ src/sortingalgo/BubbleSort.java
@@ -9,11 +9,13 @@ public class BubbleSort {	// brute force approach for sorting == bubble sort
 					int temp=a[i];
 					a[i]=a[i+1];
 					a[i+1]=temp;
+					// this is comment
 				}
 			}
 		}
 		for(int i=0;i<a.length;i++) {
 			System.out.println(a[i]);
+			System.out.println("");
 		}
 
 	}
