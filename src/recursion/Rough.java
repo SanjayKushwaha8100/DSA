340aef5973baf37d78fdc8d180d352f81137f491
goluk <goluk@DESKTOP-JMIKSHT>
2024-05-02 23:51:04 +0530

    N queen and merge sort

---
+++ src/recursion/Rough.java
@@ -0,0 +1,28 @@
+package recursion;
+
+import java.util.*;
+
+public class Rough {
+	List<List<String>> twoDList=new ArrayList<>();
+	
+	int n=4;
+	
+	ArrayList<String>temp=new ArrayList<>();
+	for(int i=0;i<n;i++) {
+		temp.add(".");
+	}
+	for(int i=0;i<n;i++) {
+		twoDList.add(temp);
+	}
+
+
+	public static void main(String[] args) {
+
+		// inserting .
+//		System.out.println(twoDList);
+		
+		
+		
+	}
+
+}
