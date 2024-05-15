0d8ecaf7b2152ec95daac004d49ac08e81166dc5
goluk <goluk@DESKTOP-JMIKSHT>
2024-05-16 00:21:14 +0530

    tree

---
+++ src/tree/TreeFromPreorderSequence.java
@@ -0,0 +1,37 @@
+package tree;
+
+public class TreeFromPreorderSequence {
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data=data;
+			left=null;
+			right=null;
+		}
+	}
+	static class BinaryTree{
+		static int idx=-1;	 // Declared at class lvl for global access 
+		public static Node buildTree(int nodesArray[]) {
+			idx++;	// at start the idx will become 0 and keep on maintaining the indx for array & indx++ for recursive calls
+			if(nodesArray[idx]==-1) {
+				return null;// reached leaf node
+			}
+			Node newNode = new Node(nodesArray[idx]);
+			newNode.left=buildTree(nodesArray);	// passing the array to build the left part of tree;
+			newNode.right=buildTree(nodesArray); // passing the array to build the right part of tree;
+			return newNode;
+		}
+	}
+
+	public static void main(String[] args) {
+		// Pre Order
+		int nodesArray[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		BinaryTree tree= new BinaryTree();
+		Node root = tree.buildTree(nodesArray);
+		System.out.println(root.data);
+
+	}
+
+}
